97f48e3bc0ac219eb6cffd699fabb19832a076c0
mayi <mayi@wiredcraft.com>
2020-04-20 07:23:17 +0800

    remove the cssnano dependence

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
     tailwindcss,
-    cssnano({
-      preset: 'default',
-    }),
     autoprefixer,
-    purgecss({
-      content: ['./src/**/*.html'],
-    }),
   ],
 };
